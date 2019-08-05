903c464817e3a63add6a12134c690746eea4ae1b
fernando <fernandoguerreroc65@gmail.com>
2019-08-04 21:38:51 -0500

    first commit

---
+++ build/precache-manifest.71ba7c412f74dcd885de37d085d8d574.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddf23e35c9f23da142154737c7b428b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9090037c5a4bec06b2e8",
+    "url": "/static/js/2.ed1ce153.chunk.js"
+  },
+  {
+    "revision": "72bc5d79bfa7f9778904",
+    "url": "/static/js/main.1b736c10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
